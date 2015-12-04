6e65694f504e6b6bf84533210b06a9b83fc9c19e
1407354 <1407354@Y350SDP20.ad.dundeeandangus.ac.uk>
2015-12-04 10:01:36 +0000

    01 start

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -18,6 +18,7 @@ public class JavaApplication8 {
     public static void main(String[] args) {
         // TODO code application logic here
         // add a commit
+        System.out.println("hello world");
     }
     
 }
